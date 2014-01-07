48c1fd9915b962b74eb2443c81bf5e3d17965acb
Andrei Gherzan <andrei@gherzan.ro>
2014-01-07 12:21:33 +0200

    Compiled on Fedora x86-64

---
+++ lib/gcc/arm-none-eabi/4.7.3/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../../gcc-4.7-2013.05/configure --prefix=/home/agherzan/arm-cs-tools --with-pkgversion='32-bit ARM EABI Toolchain JBS-2013.05-23-v2011.09-69-41-gd36d291' --with-bugurl=https://github.com/jsnyder/arm-eabi-toolchain --target=arm-none-eabi --enable-languages=c,c++ --with-gnu-ld --with-gnu-as --with-newlib --disable-nls --disable-libssp --disable-shared --enable-threads --with-headers=yes --disable-libmudflap --disable-libgomp --enable-lto --disable-libstdcxx-pch --enable-poison-system-directories --with-sysroot=/home/agherzan/arm-cs-tools/arm-none-eabi --with-build-time-tools=/home/agherzan/arm-cs-tools/arm-none-eabi/bin --enable-extra-sgxxlite-multilibs --with-specs='%{save-temps: -fverbose-asm} -D__CS_SOURCERYGXX_MAJ__=2013 -D__CS_SOURCERYGXX_MIN__=05 -D__CS_SOURCERYGXX_REV__=23 %{O2:%{!fno-remove-local-statics: -fremove-local-statics}} %{O*:%{O|O0|O1|O2|Os:;:%{!fno-remove-local-statics: -fremove-local-statics}}}' --with-gmp=/home/agherzan/work/personal/pebble-dev/arm-eabi-toolchain//libs/ --with-mpfr=/home/agherzan/work/personal/pebble-dev/arm-eabi-toolchain//libs/ --with-mpc=/home/agherzan/work/personal/pebble-dev/arm-eabi-toolchain//libs/";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
